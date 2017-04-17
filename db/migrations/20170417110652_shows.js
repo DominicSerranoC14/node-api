e7926969ff932a8308f55cd7c9812e20bb71f838
Dominic Serrano <dmsrojo@gmail.com>
2017-04-17 11:35:39 -0500

    Added shows migration file

---
+++ db/migrations/20170417110652_shows.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.up = (knex, Promise) => (
+  knex.schema.createTable('shows', (table) => {
+    table.increments();
+    table.string('name').notNullable().unique();
+    table.string('channel').notNullable();
+    table.string('genre').notNullable();
+    table.boolean('inProduction').notNullable();
+  })
+  .createTable('favorites', (table) => {
+    table.increments();
+    table.timestamp('dateAdded').notNullable().defaultTo(knex.fn.now());
+    table.integer('show_id').unsigned().references('shows_id');
+  })
+);
+
+exports.down = (knex, Promise) => knex.dropTable('shows').dripTable('favorites');
